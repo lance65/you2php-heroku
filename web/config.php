b91e07dc53c85c2117b68f2d2796cebd58c66a85
lance65 <30395872+lance65@users.noreply.github.com>
2018-03-31 11:20:13 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
+define('APIKEY', AIzaSyDLvc70PXVgl0gj-4Hc7tvNhhKjG40G6qE("APIKEY"));
 define('GJ_CODE', getenv("GJ_CODE"));
 define('SITE_NAME',getenv("SITE_NAME"));
 define('TITLENAME', getenv("TITLENAME"));
